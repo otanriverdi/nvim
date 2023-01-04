a07540e69e7bf24089b64db832fda6ee7aae72b0
Ozgur <ozgur.tanriverdi94@gmail.com>
2023-01-04 13:44:11 +0300

    remove unnecessary autocmd and refactor harpoon mappings

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,14 +1,6 @@
 -- autocmds
 local autocmd = vim.api.nvim_create_autocmd
 
--- dont list quickfix buffers
-autocmd("FileType", {
-  pattern = "qf",
-  callback = function()
-    vim.opt_local.buflisted = false
-  end,
-})
-
 -- fast yank hightlight
 local yank_group = vim.api.nvim_create_augroup("HighlightYank", {})
 autocmd("TextYankPost", {
--- lua/plugins/config/harpoon.lua
+++ lua/plugins/config/harpoon.lua
@@ -1,21 +1,5 @@
 return {
   "ThePrimeagen/harpoon",
-  keys = {
-    "<leader>ma",
-    "<leader>mr",
-    "<leader>mf",
-    "<TAB>",
-    "<S-Tab>",
-    "<leader>1",
-    "<leader>2",
-    "<leader>3",
-    "<leader>4",
-    "<leader>5",
-    "<leader>6",
-    "<leader>7",
-    "<leader>8",
-    "<leader>9",
-  },
   init = function()
     local mappings = {
       n = {
@@ -24,17 +8,13 @@ return {
         ["<leader>mf"] = { ":lua require('harpoon.ui').toggle_quick_menu()<CR>", "toggle harpoon menu" },
         ["<TAB>"] = { ":lua require('harpoon.ui').nav_next()<CR>", "jump to next harpoon file" },
         ["<S-Tab>"] = { ":lua require('harpoon.ui').nav_prev()<CR>", "jump to next harpoon file" },
-        ["<leader>1"] = { ":lua require('harpoon.ui').nav_file(1)<CR>", "jump to harpoon file 1" },
-        ["<leader>2"] = { ":lua require('harpoon.ui').nav_file(2)<CR>", "jump to harpoon file 2" },
-        ["<leader>3"] = { ":lua require('harpoon.ui').nav_file(3)<CR>", "jump to harpoon file 3" },
-        ["<leader>4"] = { ":lua require('harpoon.ui').nav_file(4)<CR>", "jump to harpoon file 4" },
-        ["<leader>5"] = { ":lua require('harpoon.ui').nav_file(5)<CR>", "jump to harpoon file 5" },
-        ["<leader>6"] = { ":lua require('harpoon.ui').nav_file(6)<CR>", "jump to harpoon file 6" },
-        ["<leader>7"] = { ":lua require('harpoon.ui').nav_file(7)<CR>", "jump to harpoon file 7" },
-        ["<leader>8"] = { ":lua require('harpoon.ui').nav_file(8)<CR>", "jump to harpoon file 8" },
-        ["<leader>9"] = { ":lua require('harpoon.ui').nav_file(9)<CR>", "jump to harpoon file 9" },
       },
     }
+
+    for i = 1, 9 do
+      mappings.n[string.format("<leader>%s", i)] =
+        { string.format(":lua require('harpoon.ui').nav_file(%s)<CR>", i), string.format("jump to harpoon file %s", i) }
+    end
 
     require("core.utils").load_mappings(mappings)
   end,
