b451a522ffd9eee8641cc19415627294db3ec59d
Ozgur <ozgur.tanriverdi94@gmail.com>
2023-01-15 17:07:04 +0300

    replaced vim surround with nvim variant

--- lua/plugins/config/surround.lua
+++ lua/plugins/config/surround.lua
@@ -1,4 +1,9 @@
 return {
-  "tpope/vim-surround",
+  "kylechui/nvim-surround",
   event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("nvim-surround").setup({
+      -- Configuration here, or leave empty to use defaults
+    })
+  end,
 }
